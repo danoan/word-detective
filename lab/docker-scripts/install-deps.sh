1106c75a069d9703ca211cee900daa87611e16a1
DanielMA <danoan2008@gmail.com>
2021-10-24 11:54:15 +0200

    Refactor of build scripts
    This commit refactors some scripts run to build
    the project in a docker container. Among the modifications
    
    -  Refactor of build-install.sh
    -  Manual installation of boost
    -  Add a flag such that one can opt to generate coverage
    files or not
    
    Refactor travis build scripts
    
    This commit splits the docker file used to build
    the project in two, one for binaries (dockerfile.bins,
    which is used to build a node.js server) and another
    for testing purposes (dockerfile.travis).

--- lab/docker-scripts/install-deps.sh
+++ lab/docker-scripts/install-deps.sh
@@ -4,6 +4,16 @@ SOURCE_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 PROJECT_ROOT="$(cd "${SOURCE_DIR}" && cd ../.. && pwd)"
 EXT_DIR="${PROJECT_ROOT}/ext"
 
+# Boost
+
+wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2 -O boost.tar.bz2
+tar x -jf boost.tar.bz2
+
+pushd boost_1_76_0 1> /dev/null
+./bootstrap.sh --with-libraries=filesystem,system --prefix=/usr/local
+./b2 install
+popd 1> /dev/null
+
 # Install catch
 
 # Install json/nlohmann
--- lab/docker-scripts/run-tests.sh
+++ lab/docker-scripts/run-tests.sh
@@ -3,6 +3,7 @@
 SOURCE_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 PROJECT_ROOT="$(cd "${SOURCE_DIR}" && cd ../.. && pwd)"
 BUILD_DIR="${PROJECT_ROOT}/build"
+GENERATE_COVERAGE_FILES="${1:-no-generation-coverage-files}"
 
 GCOV_FILES="${BUILD_DIR}/lab/test/coverage/CMakeFiles/test-coverage.dir"
 COVERAGE_OUTPUT_FOLDER="${PROJECT_ROOT}/lab/test/coverage/output"
@@ -10,9 +11,12 @@ COVERAGE_OUTPUT_FOLDER="${PROJECT_ROOT}/lab/test/coverage/output"
 "${PROJECT_ROOT}/install/lab/test-coverage"
 mkdir -p "${COVERAGE_OUTPUT_FOLDER}"
 
-gcov "${PROJECT_ROOT}/lab/test/coverage/test-coverage.cpp" --object-directory "${GCOV_FILES}"
+if [ ${GENERATE_COVERAGE_FILES} == '--generate-coverage-files' ]
+then
+  gcov "${PROJECT_ROOT}/lab/test/coverage/test-coverage.cpp" --object-directory "${GCOV_FILES}"
 
-lcov --capture --directory "${GCOV_FILES}" --output-file "${COVERAGE_OUTPUT_FOLDER}/test_coverage.info"
-# genhtml "${COVERAGE_OUTPUT_FOLDER}/test_coverage.info" --output-directory "${COVERAGE_OUTPUT_FOLDER}/report"
+  lcov --capture --directory "${GCOV_FILES}" --output-file "${COVERAGE_OUTPUT_FOLDER}/test_coverage.info"
+  # genhtml "${COVERAGE_OUTPUT_FOLDER}/test_coverage.info" --output-directory "${COVERAGE_OUTPUT_FOLDER}/report"
 
-bash <(curl -s https://codecov.io/bash)
+  bash <(curl -s https://codecov.io/bash)
+fi
