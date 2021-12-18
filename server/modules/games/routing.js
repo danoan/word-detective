86e0af762f4f4490db8f0cb42c541651b7bcef86
DanielMA <danoan2008@gmail.com>
2021-12-18 16:54:28 +0100

    [server] Escape backticks

--- server/modules/games/routing.js
+++ server/modules/games/routing.js
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 import md5 from 'md5'
+import { Transform } from 'stream';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PROJECT_ROOT = path.resolve(__dirname, "../..");
@@ -8,14 +9,16 @@ const PROJECT_ROOT = path.resolve(__dirname, "../..");
 export let routing = function () {
 
   function fromFile(req, res) {
-    let text = req.files.text_file.data.toString();
+    //replaceAll is not implemented in this version of nodejs
+    let text = req.files.text_file.data.toString().replace(/`/g,'\\`');
     let checksum = md5(text);
 
     res.render('games/puzzle-from-text/index.ntl', { vars: { text, checksum } });
   }
 
   function fromString(req, res) {
-    let text = req.body.text;
+    //replaceAll is not implemented in this version of nodejs
+    let text = req.body.text.replace(/`/g,'\\`');
     let checksum = md5(text);
 
     res.render('games/puzzle-from-text/index.ntl', { vars: { text, checksum } });
